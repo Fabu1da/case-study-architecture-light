00a92ab5bd4107f82e7878556cb7e2042eab066d
Fabu1da <fabiusyy@gmail.com>
2026-05-27 15:00:23 +0200

    Define User entity with TypeORM for users table

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -1,7 +1,19 @@
-// /* eslint-disable indent */
-// import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
-// @Entity({ name: 'users' })
-// export class User {
-// 
-// }
+ @Entity({ name: 'users' })
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ length: 100 })
+    firstName: string;
+
+    @Column({ length: 100 })
+    lastName: string;
+
+    @Column({ unique: true, length: 100 })
+    email: string;
+
+    @Column()
+    password: string;
+}
